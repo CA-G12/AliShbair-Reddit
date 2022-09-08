fba2dcdfac095810d55e988703347f03328d1328
ShbairAli <ali89freelancer@gmail.com>
2022-09-08 21:07:51 +0300

    signin route tested by frontend and working fine relates #13

--- server/controllers/logControllers/postSignin.js
+++ server/controllers/logControllers/postSignin.js
@@ -4,10 +4,7 @@ const getUserByEmail = require('../../database/queries/logQueries/getUserByEmail
 const postSignin = (req, res) => {
     console.log('postSignin');
     console.log(req.body);
-    postSigninQuery()
-        .then(data => console.log('DB Users:', data.rows[1].username))
-        .catch(err => console.log(err))
-    res.send('you signed in!!')
+
 };
 
 module.exports = postSignin;
