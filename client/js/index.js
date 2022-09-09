7d0df4a95b0b543efb6faf749938ccd5704d65ad
ShbairAli <ali89freelancer@gmail.com>
2022-09-09 03:27:45 +0300

    printed user email on homepage if loggedin and token verified relates #17

--- client/js/index.js
+++ client/js/index.js
@@ -1,7 +1,9 @@
+const greetedEmail = document.querySelector('.greeted-email');
+
 const greetUser = () => {
     fetch('/greet')
         .then(data => data.json())
-        .then(userEmail => alert(userEmail.email))
+        .then(userEmail => greetedEmail.textContent = `Welcome: ${userEmail.email}`)
         .catch(err => alert(err.msg))
 }
 
--- server/utils/verifyToken.js
+++ server/utils/verifyToken.js
@@ -11,7 +11,9 @@ module.exports = verifyToken = (req, res, next) => {
         jwt.verify(validToken, process.env.SECRET_KEY, (err, decoded) => {
             if (err) {
                 console.log('sorry, token is manipulated!');
+                // res.end();
                 throw new ExtendedError('Token is being manipulated!', 400);
+
             } else {
                 console.log('DECODED INFO:', decoded);
                 req.userEmail = decoded.email;
