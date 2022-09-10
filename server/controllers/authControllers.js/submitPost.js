5bd69e44144229b6bf8a4b9df6a6f2813cbe4b17
ShbairAli <ali89freelancer@gmail.com>
2022-09-10 05:34:24 +0300

    validated post, authenticated user token

--- server/controllers/authControllers.js/submitPost.js
+++ server/controllers/authControllers.js/submitPost.js
@@ -1,7 +1,20 @@
 const submitPostQuery = require('../../database/queries/authQueries/submitPostQuery');
+const getUserByEmail = require('../../database/queries/logQueries/getUserByEmail');
+const { validatePost } = require('../../utils/validate');
+const ExtendedError = require('../../utils/ExtendedError');
 
-const submitPost = (req, res) => {
-    console.log('comment');
+const submitPost = (req, res, next) => {
+    try {
+        console.log('submitPost');
+        const { error } = validatePost(req.body);
+        if (error) throw new ExtendedError(error.details[0].message, 400);
+        console.log('Validated submit post yeah', req.body);
+    } catch (err) {
+        next(err);
+    }
+
+    
+
 };
 
 module.exports = submitPost;
